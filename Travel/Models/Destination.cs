4ce98ff2772f96ce54935abf7e13b54143e2bd8d
Jeff Terrell <jeffaterrell@gmail.com>
2022-01-18 17:38:04 -0500

    add models, migrations, and update boilerplate for Startup.cs
    Co-Authored-By: Colt Thatcher <colt.thatcher303@gmail.com>
    Co-Authored-By : Kate Kiatsiri <keidsiri@gmail.com>

---
+++ Travel/Models/Destination.cs
@@ -0,0 +1,10 @@
+namespace Travel.Models
+{
+  public class Destination
+  {
+    public int DestinationId { get; set; }
+    public string Name { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+  }
+}
